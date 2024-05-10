7a20e256ccd07f4d16a35925ed3ae7b9d0e0da42
Asa G <asagauntlett1@gmail.com>
2024-05-10 15:47:57 +0100

    10/05

---
+++ MATLABCode/customBubbleSort.m
@@ -0,0 +1,45 @@
+function [sortedInputList, counter] = customBubbleSort(INPUTLIST, ORDERLIST, M)
+    % Define symbolic variable for computations
+    syms k1 k2 k3 l1 l2 l3;
+
+    % Initialize the symbolic counter
+    counter = sym(0);
+
+    % Modify ORDERLIST by removing '1' and duplicating remaining elements
+    modifiedOrderList = repmat(ORDERLIST(ORDERLIST ~= 1), 1, 2);
+
+    % Initialize the sorted list as the INPUTLIST initially
+    sortedInputList = INPUTLIST;
+
+    % Convert sortedInputList to an array of labels for easier processing
+    labels = cellfun(@(x) x(1), sortedInputList);
+    expressions = cellfun(@(x) x(2), sortedInputList, 'UniformOutput', false);
+
+    % Perform the bubble sort
+    n = length(labels);
+    for i = 1:n
+        madeSwap = false;
+        for j = 1:n-i
+            % Find index of labels[j] and labels[j+1] in modifiedOrderList
+            currentLabelIndex = find(modifiedOrderList == labels(j), 1);
+            nextLabelIndex = find(modifiedOrderList == labels(j+1), 1);
+            
+            % Swap if current label comes after the next label in order list
+            if currentLabelIndex > nextLabelIndex
+                % Update counter with the product of matrix entry and the expressions
+                matrixValue = M(labels(j), labels(j+1));
+                counter = counter + matrixValue * expressions{j} * expressions{j+1};
+                
+                % Perform the swap
+                [sortedInputList{j}, sortedInputList{j+1}] = deal(sortedInputList{j+1}, sortedInputList{j});
+                [labels(j), labels(j+1)] = deal(labels(j+1), labels(j));
+                [expressions(j), expressions(j+1)] = deal(expressions(j+1), expressions(j));
+                madeSwap = true;
+            end
+        end
+        % Break early if no swaps were made in this pass
+        if ~madeSwap
+            break;
+        end
+    end
+end
--- MATLABCode/main2.m
+++ MATLABCode/main2.m
@@ -1,8 +1,22 @@
-tic;
+% Define the adjacency matrix M
+M = [0 1 1; -1 0 -1; -1 1 0;];
 
-M = [0 1 0; -1 0 -1; 0 1 0];
-y = deriveflavourvectorONEFS(M);
-disp('Solution vector coefficients:');
-disp(y);
 
-toc;
+% Define symbolic expressions (assuming k1, k2, k3, l1, l2, l3 are defined)
+exp1 = str2sym('k1 - l1');
+exp2 = str2sym('-k1 + l1');
+exp3 = str2sym('-k3 + l3');
+exp4 = str2sym('-k2 + l2');
+
+% Define INPUTLIST with labels and expressions
+INPUTLIST = {[2, exp1], [3, exp2], [3, exp3], [2, exp4]};
+
+% Define ORDERLIST with labels
+ORDERLIST = [1, 3, 2];
+
+% Call the customBubbleSortWithMatrix function
+[sortedInputList, counter] = customBubbleSort(INPUTLIST, ORDERLIST, M);
+
+% Display the sorted list and the counter
+disp(sortedInputList);
+disp(counter);
