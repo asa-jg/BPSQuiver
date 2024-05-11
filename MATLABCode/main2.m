b80c1f1121c0035b28b700644deb1a61a91a1966
Asa G <asagauntlett1@gmail.com>
2024-05-11 15:34:38 +0100

    deriveflavourvector added

--- MATLABCode/main2.m
+++ MATLABCode/main2.m
@@ -1,22 +1,7 @@
-% Define the adjacency matrix M
-M = [0 1 1; -1 0 -1; -1 1 0;];
-
-
-% Define symbolic expressions (assuming k1, k2, k3, l1, l2, l3 are defined)
-exp1 = str2sym('k1 - l1');
-exp2 = str2sym('-k1 + l1');
-exp3 = str2sym('-k3 + l3');
-exp4 = str2sym('-k2 + l2');
-
-% Define INPUTLIST with labels and expressions
-INPUTLIST = {[2, exp1], [3, exp2], [3, exp3], [2, exp4]};
-
-% Define ORDERLIST with labels
-ORDERLIST = [1, 3, 2];
-
-% Call the customBubbleSortWithMatrix function
-[sortedInputList, counter] = customBubbleSort(INPUTLIST, ORDERLIST, M);
-
-% Display the sorted list and the counter
-disp(sortedInputList);
-disp(counter);
+% Define an antisymmetric matrix with a guaranteed null space of dimension >= 2
+M1 = [0 -1 -1 -1 -1; 1 0 0 0 0; 1 0 0 0 0; 1 0 0 0 0; 1 0 0 0 0;];
+M2 = [0 0 -1 -1; 0 0 -1 -1; 1 1 0 0; 1 1 0 0];
+plotgraph(M2);
+Y = solveLinearSystem(M2);
+disp('Independent solution vectors:');
+disp(Y);
